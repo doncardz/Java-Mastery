2bc522eee3b283137315fe219e216f12338bb96b
doncardz <carloventocilla11@gmail.com>
2026-04-21 16:09:26 +0800

    init commit

---
+++ src/com/javaupskill/oop_basics/classes_object/Main.java
@@ -0,0 +1,24 @@
+package com.javaupskill.oop_basics.classes_object;
+
+class Student {
+    String name;
+    int age;
+    String course;
+
+    void introduce() {
+        System.out.println("Hello I am " + name);
+        System.out.println("I am " + age + "yrs old.");
+        System.out.println("I am " + course + " student.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.name = "Carlo";
+        student.age = 19;
+        student.course = "BSIT";
+        student.introduce();
+    }
+}
---
+++ src/com/javaupskill/oop_basics/classes_object/multipleObjects.java
@@ -0,0 +1,40 @@
+package com.javaupskill.oop_basics.classes_object;
+
+class Students{
+    String name;
+    int age;
+    String course;
+
+    void introduce() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course " + course);
+        System.out.println("===================");
+    }
+}
+
+public class multipleObjects {
+    public static void main(String[] args){
+
+        Students student = new Students();
+        student.name = "Carlo";
+        student.age = 19;
+        student.course = "BSIT";
+        student.introduce();
+
+        Students student1 = new Students();
+        student1.name = "Locar";
+        student1.age = 20;
+        student1.course = "BSCS";
+        student1.introduce();
+
+        Students student2 = new Students();
+        student2.name = "Don";
+        student2.age = 21;
+        student2.course = "BSCpE";
+        student2.introduce();
+
+
+
+    }
+}
