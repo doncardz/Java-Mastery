ffc648a8d495e3c347008ea09a47d82bcdf6c538
doncardz <carloventocilla11@gmail.com>
2026-04-23 19:27:57 +0800

    init commit

---
+++ src/com/javaupskill/oop_basics/finalKeyword/finalClass/Main.java
@@ -0,0 +1,19 @@
+package com.javaupskill.oop_basics.finalKeyword.finalClass;
+
+//ang final Class ay bawal na maging parent class
+
+final class Animal{
+    void sound() {
+        System.out.println("tweet");
+    }
+}
+/*
+class Bird extends Animal {
+
+}
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
