b380473eba334f752c9edab88fced090ee75391a
User <carloventocilla11@gmail.com>
2026-05-03 19:16:17 +0800

    init commit

--- src/com/javaupskill/fundametals/dataTypes/nonPrimitive/string/StringMethods.java
+++ src/com/javaupskill/fundametals/dataTypes/nonPrimitive/string/StringMethods.java
@@ -1,4 +1,11 @@
 package com.javaupskill.fundametals.dataTypes.nonPrimitive.string;
 
 public class StringMethods {
+    public static void main(String[] args) {
+        String name = "Carlo";
+
+        System.out.println(name.length());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
 }
