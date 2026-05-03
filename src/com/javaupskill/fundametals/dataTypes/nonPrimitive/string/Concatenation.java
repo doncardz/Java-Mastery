446f4b711c174f5e4f30cd29d2a7ea7b3d1560ba
User <carloventocilla11@gmail.com>
2026-05-03 19:21:40 +0800

    init commit

---
+++ src/com/javaupskill/fundametals/dataTypes/nonPrimitive/string/Concatenation.java
@@ -0,0 +1,10 @@
+package com.javaupskill.fundametals.dataTypes.nonPrimitive.string;
+
+public class Concatenation {
+    public static void main(String[] args) {
+        String fname = "Carlo";
+        String lname = "Ventocilla";
+
+        System.out.println(fname + " " + lname);
+    }
+}
---
+++ src/com/javaupskill/fundametals/dataTypes/nonPrimitive/string/RealUsage.java
@@ -0,0 +1,11 @@
+package com.javaupskill.fundametals.dataTypes.nonPrimitive.string;
+
+public class RealUsage {
+    public static void main(String[] args) {
+        String name = "Carlo";
+        int age =  19;
+
+        System.out.println("Hi, My name is " + name + ".");
+        System.out.println("My age is " + age + ".");
+    }
+}
