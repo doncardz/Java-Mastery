ae3bd61fe75ce0c8e30602cb191657e6f315539d
User <carloventocilla11@gmail.com>
2026-05-05 18:05:23 +0800

    added Explicit

---
+++ src/com/javaupskill/fundametals/typeCasting/NarrowCast.java
@@ -0,0 +1,10 @@
+package com.javaupskill.fundametals.typeCasting;
+
+public class NarrowCast {
+    public static void main(String[] args) {
+        double myDouble = 111.11;
+        int myInt = (int) myDouble;
+
+        System.out.println(myInt);
+    }
+}
