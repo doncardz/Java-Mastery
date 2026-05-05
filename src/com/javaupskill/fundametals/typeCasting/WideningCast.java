ac7cd9baf7da9d239a714b0701f0b97ff98f5e89
User <carloventocilla11@gmail.com>
2026-05-05 18:01:44 +0800

    added Implicit

---
+++ src/com/javaupskill/fundametals/typeCasting/WideningCast.java
@@ -0,0 +1,11 @@
+package com.javaupskill.fundametals.typeCasting;
+
+//Widening Cast or Implicit
+public class WideningCast {
+    public static void main(String[] args) {
+        int myInt = 19;
+        double myDouble = myInt;
+
+        System.out.println(myDouble);
+    }
+}
