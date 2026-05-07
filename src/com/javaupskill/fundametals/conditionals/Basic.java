096978ebbd1ad6c85c8e1ef6b21412e4ca01b3f9
Carlo Laptop <carloventocilla11@gmail.com>
2026-05-07 19:46:38 +0800

    added conditional package and basics

---
+++ src/com/javaupskill/fundametals/conditionals/Basic.java
@@ -0,0 +1,13 @@
+package com.javaupskill.fundametals.conditionals;
+
+public class Basic {
+    public static void main(String[] args) {
+        int age = 19;
+
+        if(age >= 18) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are still a minor");
+        }
+    }
+}
