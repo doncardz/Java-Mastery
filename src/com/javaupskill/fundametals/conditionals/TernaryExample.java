ed621b58f67da76027633a549bad646198d2285e
Carlo Laptop <carloventocilla11@gmail.com>
2026-05-07 20:27:32 +0800

    added Ternary operator

---
+++ src/com/javaupskill/fundametals/conditionals/TernaryExample.java
@@ -0,0 +1,10 @@
+package com.javaupskill.fundametals.conditionals;
+
+public class TernaryExample {
+    public static void main(String[] args) {
+        int age = 17;
+        String result = (age >= 18) ? "Adult" : "Minor";
+
+        System.out.println(result);
+    }
+}
